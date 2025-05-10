3a38685b629da3d5f5387dc29dc39e394e071af7
divyalakshana01 <divyalakshanar@gmail.com>
2025-05-09 21:27:07 -0700

    Updated bullseye.js

--- m05/js/bullseye.js
+++ m05/js/bullseye.js
@@ -1,1 +1,28 @@
+// Prompt user for input and convert it to Number
+var table;
 
+do {
+   table = prompt("Please enter a number (0 to 10): ");
+   table = Number(table);
+} while (table < 0 || table > 10);
+
+// Validate input and set default value
+if (table === null || Number.isNaN(table)) {
+   table = 0;
+}
+
+// Set counter to 1
+var i = 1;
+
+// Set Message                 
+var msg = '<h2>Multiplication Table</h2>';
+
+// Do multiplication
+while (i < 11) {
+   msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+   i++;
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
