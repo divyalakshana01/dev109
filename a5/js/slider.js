4d148b803079a33396a6fe492cc5613ab06bcd44
divyalakshana01 <divyalakshanar@gmail.com>
2025-06-14 22:19:30 -0700

    Updated slider.js

--- a5/js/slider.js
+++ a5/js/slider.js
@@ -1,1 +1,68 @@
+// image slider
+const slides = document.querySelectorAll(".slides img");
+const timerDisplay = document.getElementById("slider-timer");
 
+var slideIndex = 0;
+var intervalId = null;
+var countdown = 4;
+var countdownIntervalId = null;
+
+initializeSlider();
+
+function initializeSlider(){
+	if (slides.length > 0) {
+		slides[slideIndex].classList.add("displaySlide");
+		startTimer();
+	}
+}
+function startTimer(){
+	countdown = 4;
+	updateTimerDisplay();
+	intervalId = setInterval(() => {
+		nextSlide();
+		countdown = 4;
+	}, 4000);
+
+	countdownIntervalId = setInterval(() => {
+		countdown--;
+		updateTimerDisplay();
+	}, 1000);	
+}
+
+function updateTimerDisplay() {
+	timerDisplay.textContent = countdown;
+}
+
+
+function showSlide(index){
+	if(index >= slides.length){
+		slideIndex = 0;
+	}
+	else if(index < 0){
+		slideIndex = slides.length -1;
+	}
+
+	slides.forEach(slide => {
+		slide.classList.remove("displaySlide");
+	});
+	slides[slideIndex].classList.add("displaySlide");
+
+}
+
+function prevSlide(){
+	resetTimers();
+	slideIndex--;
+	showSlide(slideIndex);
+	startTimer();
+
+}
+
+function nextSlide(){
+	slideIndex++;
+	showSlide(slideIndex);
+}
+
+function resetTimers() {
+	clearInterval(intervalId);
+	clearInterval(countdownIntervalId);
+}
