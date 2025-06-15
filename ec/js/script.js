d078efcfaea30feb4029a2c9c4747660b4ca9179
divyalakshana01 <divyalakshanar@gmail.com>
2025-06-14 23:25:10 -0700

    Updated script.js

--- ec/js/script.js
+++ ec/js/script.js
@@ -1,1 +1,33 @@
+const found = {
+    tissue: false,
+    clock: false,
+    button: false
+  };
 
+  function foundItem(name) {
+    if (!found[name]) {
+      found[name] = true;
+      document.getElementById(name).style.textDecoration = "line-through";
+
+      if (Object.values(found).every(v => v)) {
+        setTimeout(() => {
+          if (confirm("You found all characters! Play again?")) {
+            replay();
+          } else {
+            document.getElementById("replay").style.display = "inline-block";
+          }
+        }, 300);
+      }
+    }
+  }
+
+  document.querySelectorAll("area").forEach(area => {
+    area.addEventListener("click", function (event) {
+      event.preventDefault();
+      foundItem(area.alt);
+    });
+  });
+
+  function replay() {
+    location.reload();
+  }
